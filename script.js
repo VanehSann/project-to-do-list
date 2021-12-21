fb679c39b4814868d85f17320aa55deae8edba7c
VanehSann <vanessacbsantos22@gmail.com>
2021-12-21 19:44:30 -0300

    script 1 ao 11 - com lint corrigido

--- script.js
+++ script.js
@@ -11,10 +11,55 @@ criarTarefa.onclick = function click() {
   document.body.appendChild(listaTarefas);
   textoTarefa.value = '';
 };
-// referência de maneiras de usar o forEach:
-// em = https://stackoverflow.com/questions/65530318/add-remove-class-when-clicking-on-element-with-pure-javascript
-// let numero = 0;
-// document.querySelector('li').addEventListener('click', function unnamed() {
-//   numero += 1;
-//   return numero;
-// });
+// https://developer.mozilla.org/en-US/docs/Web/API/Element/dblclick_event
+// https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_todo
+function doisClick(event) {
+  if (event.target.tagName === 'LI') {
+    event.target.classList.toggle('completed');
+  }
+}
+const completedLine = document.querySelector('ol');
+completedLine.addEventListener('dblclick', doisClick);
+// inpirei no projeto anterior, mesmo codigo
+function limpa() {
+// teste
+  const lista = document.querySelectorAll('.lista');
+  for (let i = 0; i < lista.length; i += 1) {
+    lista[i].remove();
+  }
+}
+//
+// inpirei no projeto anterior, mesmo codigo
+function removerFinalizados() {
+// teste
+  const completed = document.querySelectorAll('.completed');
+  for (let i = 0; i < completed.length; i += 1) {
+    completed[i].remove();
+  }
+}
+// const completedLine = document.querySelector('ol');
+function umClick(event) {
+  if (event.target.tagName === 'LI') {
+    const listas = document.querySelectorAll('.lista');
+    listas.forEach((i) => {
+      i.addEventListener('click', function semNomeDois() {
+        listas.forEach((j) => j.classList.remove('selected'));
+        this.classList.add('selected');
+      });
+    });
+  }
+}
+completedLine.addEventListener('click', umClick);
+function removeSelected() {
+  // teste
+  const listaSelected = document.querySelectorAll('.selected');
+  for (let i = 0; i < listaSelected.length; i += 1) {
+    listaSelected[i].remove();
+  }
+}
+// limpar lint
+window.onload = function unNameFunc() {
+  limpa();
+  removeSelected();
+  removerFinalizados();
+};
