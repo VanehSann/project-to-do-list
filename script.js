539c618b934f75fb16acd618e72f37a24fb33759
VanehSann <vanessacbsantos22@gmail.com>
2022-09-12 01:10:19 -0300

    refactor: remove trybe files

--- script.js
+++ script.js
@@ -2,11 +2,6 @@ const textoTarefa = document.getElementById('texto-tarefa');
 const criarTarefa = document.getElementById('criar-tarefa');
 const listaTarefas = document.getElementById('lista-tarefas');
 // teste
-const completedLine = document.querySelector('ol');
-const completedLiner = document.querySelector('ol');
-function salvarTarefas() {
-  localStorage.setItem('listaSalva', JSON.stringify(completedLiner.innerHTML));
-}
 criarTarefa.onclick = function click() {
   const lista = document.createElement('li');
   lista.className = 'lista';
@@ -17,6 +12,25 @@ criarTarefa.onclick = function click() {
   document.body.appendChild(listaTarefas);
   textoTarefa.value = '';
 };
+const completedLine = document.querySelector('ol');
+const completedLiner = document.querySelector('ol');
+function insertPhraseInDOM() { // inserir a frase no DOM
+  const phrasesList = JSON.parse(localStorage.getItem('listaSalva'));
+  const listLength = phrasesList.length - 1;
+  const phraseText = phrasesList[listLength];
+  const phrase = document.createElement('li');
+  phrase.innerText = phraseText;
+  listaTarefas.appendChild(phrase);
+}
+function salvarTarefas() {
+    const oldList = JSON.parse(localStorage.getItem('listaSalva'));
+    const phraseText = textoTarefa.value;
+    oldList.push(phraseText);
+    localStorage.setItem('listaSalva', JSON.stringify(oldList));
+    // insertPhraseInDOM();
+  
+}
+
 // https://developer.mozilla.org/en-US/docs/Web/API/Element/dblclick_event
 // https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_todo
 function doisClick(event) {
@@ -34,7 +48,7 @@ function limpa() {
   for (let i = 0; i < lista.length; i += 1) {
     lista[i].remove();
   }
-  salvarTarefas();
+  // salvarTarefas();
 }
 //
 // inpirei no projeto anterior, mesmo codigo
@@ -44,7 +58,7 @@ function removerFinalizados() {
   for (let i = 0; i < completed.length; i += 1) {
     completed[i].remove();
   }
-  salvarTarefas();
+  // salvarTarefas();
 }
 // the link was changed https://stackoverflow.com/questions/70456341/click-event-only-works-on-the-second-click (new link)
 // https://stackoverflow.com/questions/70456341/how-can-i-make-my-function-that-change-the-classname-when-i-click-works
@@ -62,7 +76,7 @@ function umClick(event) {
     });
   });
   // }
-  salvarTarefas();
+  // salvarTarefas();
 }
 completedLine.addEventListener('mousedown', umClick);
 function removeSelected() {
@@ -71,13 +85,25 @@ function removeSelected() {
   for (let i = 0; i < listaSelected.length; i += 1) {
     listaSelected[i].remove();
   }
-  salvarTarefas();
+  // salvarTarefas();
+}
+function initialRenderization() {
+  if (localStorage.getItem('listaSalva') === null) {
+    localStorage.setItem('listaSalva', JSON.stringify([]));
+  } else {
+    const phrasesList = JSON.parse(localStorage.getItem('listaSalva'));
+    const listLength = phrasesList.length - 1;
+    for (let index = 0; index <= listLength; index += 1) {
+      const listElement = document.createElement('li');
+      listElement.innerText = phrasesList[index];
+      listaTarefas.appendChild(listElement);
+    }
+  }
 }
 // limpar lint
 window.onload = function unNameFunc() {
-  completedLiner.innerHTML = JSON.parse(localStorage.getItem('listaSalva'));
   limpa();
   removeSelected();
   removerFinalizados();
-  salvarTarefas();
+  initialRenderization();
 };
